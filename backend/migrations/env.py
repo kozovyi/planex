4774b09e3bae1b425f1eaeb89c4aeea9e7a75b0f
Pavlo Kozovyi <p.kozoviy@gmail.com>
2025-04-14 21:30:52 +0300

    t36

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -9,7 +9,6 @@ from alembic import context
 from core.models import Base
 from core.config import settings
 
-
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
