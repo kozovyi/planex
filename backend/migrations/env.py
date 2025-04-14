919343c2c413dccd3a7e6f53d421e0b44a2864d4
Pavlo Kozovyi <p.kozoviy@gmail.com>
2025-04-14 21:36:24 +0300

    t38

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -13,7 +13,6 @@ from core.config import settings
 # access to the values within the .ini file in use.
 config = context.config
 
-
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
