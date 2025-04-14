33d3aabd3f2cc0d3fa7f4f678f46de78a263b7f8
Pavlo Kozovyi <p.kozoviy@gmail.com>
2025-04-14 20:59:03 +0300

    t27

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -17,7 +17,6 @@ async def test_register_user():
         assert "id" in response.json()
         assert response.json()["email"] == user_data["email"]
 
-
 async def test_login_user():
     async with AsyncClient(
         transport=ASGITransport(app=app),
